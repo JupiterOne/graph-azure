2a6bcfc3f7c22ce3ece47f59dd7bf63f8974ac35
Adam Williams <adam.williams@lifeomic.com>
2019-09-20 09:01:22 -0400

    Use values that will match recordings in CI

--- test/integrationInstanceConfig.ts
+++ test/integrationInstanceConfig.ts
@@ -5,9 +5,11 @@ const config: AzureIntegrationInstanceConfig = {
   clientSecret:
     process.env.AZURE_CLOUD_LOCAL_EXECUTION_CLIENT_SECRET || "clientSecret",
   directoryId:
-    process.env.AZURE_CLOUD_LOCAL_EXECUTION_DIRECTORY_ID || "directoryId",
+    process.env.AZURE_CLOUD_LOCAL_EXECUTION_DIRECTORY_ID ||
+    "a76fc728-0cba-45f0-a9eb-d45207e14513",
   subscriptionId:
-    process.env.AZURE_CLOUD_LOCAL_EXECUTION_SUBSCRIPTION_ID || "subscriptionId",
+    process.env.AZURE_CLOUD_LOCAL_EXECUTION_SUBSCRIPTION_ID ||
+    "dccea45f-7035-4a17-8731-1fd46aaa74a0",
 };
 
 export default config;

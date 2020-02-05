0f3cc504d40039d8d66761f57e10add491bbddc2
erkangz <erkang.zheng@lifeomic.com>
2020-02-05 15:24:24 -0500

    Add MySQL support

--- src/azure/resource-manager/ResourceManagerClient.ts
+++ src/azure/resource-manager/ResourceManagerClient.ts
@@ -5,6 +5,7 @@ import { IntegrationLogger } from "@jupiterone/jupiter-managed-integration-sdk";
 
 import { ComputeManagementClient } from "@azure/arm-compute";
 import { VirtualMachine } from "@azure/arm-compute/esm/models";
+
 import { NetworkManagementClient } from "@azure/arm-network";
 import {
   NetworkInterface,
@@ -12,9 +13,22 @@ import {
   PublicIPAddress,
   VirtualNetwork,
 } from "@azure/arm-network/esm/models";
+
+import { MySQLManagementClient } from "@azure/arm-mysql";
+import {
+  Server as MySQLServer,
+  Database as MySQLDatabase,
+} from "@azure/arm-mysql/esm/models";
+
 import { SqlManagementClient } from "@azure/arm-sql";
+import {
+  Server as SQLServer,
+  Database as SQLDatabase,
+} from "@azure/arm-sql/esm/models";
+
 import { StorageManagementClient } from "@azure/arm-storage";
 import { BlobContainer, StorageAccount } from "@azure/arm-storage/esm/models";
+
 import { AzureServiceClient } from "@azure/ms-rest-azure-js";
 import {
   ServiceClientCredentials,
@@ -28,7 +42,6 @@ import { resourceGroupName } from "../utils";
 import authenticate from "./authenticate";
 import { AzureManagementClientCredentials } from "./types";
 import { bunyanLogPolicy } from "./BunyanLogPolicy";
-import { Server, Database } from "@azure/arm-sql/esm/models";
 
 interface ClientConstructor<T extends AzureServiceClient> {
   new (
@@ -67,6 +80,8 @@ export default class ResourceManagerClient {
     this.clientCache = new Map();
   }
 
+  //// Compute and Network ////
+
   public async iterateNetworkInterfaces(
     callback: (nic: NetworkInterface) => void,
   ): Promise<void> {
@@ -94,6 +109,15 @@ export default class ResourceManagerClient {
     const client = await this.getAuthenticatedClient(NetworkManagementClient);
     return this.iterateAllResources(client.networkSecurityGroups, callback);
   }
+
+  public async iteratePublicIPAddresses(
+    callback: (ip: PublicIPAddress) => void,
+  ): Promise<void> {
+    const client = await this.getAuthenticatedClient(NetworkManagementClient);
+    return this.iterateAllResources(client.publicIPAddresses, callback);
+  }
+
+  //// Storage ////
 
   public async iterateStorageAccounts(
     callback: (sa: StorageAccount) => void | Promise<void>,
@@ -125,14 +149,18 @@ export default class ResourceManagerClient {
     );
   }
 
-  public async iterateSqlServers(callback: (s: Server) => void): Promise<void> {
+  //// Databases ////
+
+  public async iterateSqlServers(
+    callback: (s: SQLServer) => void,
+  ): Promise<void> {
     const client = await this.getAuthenticatedClient(SqlManagementClient);
     return this.iterateScopedResources(client.servers, callback);
   }
 
   public async iterateSqlDatabases(
-    server: Server,
-    callback: (d: Database) => void,
+    server: SQLServer,
+    callback: (d: SQLDatabase) => void,
   ): Promise<void> {
     const client = await this.getAuthenticatedClient(SqlManagementClient);
     const resourceGroup = resourceGroupName(server.id, true)!;
@@ -148,12 +176,39 @@ export default class ResourceManagerClient {
     );
   }
 
-  public async iteratePublicIPAddresses(
-    callback: (ip: PublicIPAddress) => void,
+  public async iterateMySqlServers(
+    callback: (s: MySQLServer) => void,
   ): Promise<void> {
-    const client = await this.getAuthenticatedClient(NetworkManagementClient);
-    return this.iterateAllResources(client.publicIPAddresses, callback);
+    const client = ((await this.getAuthenticatedClient(
+      MySQLManagementClient as any,
+    )) as unknown) as MySQLManagementClient;
+    const servers = await client.servers.list();
+    for (const server of servers) {
+      callback(server);
+    }
   }
+
+  public async iterateMySqlDatabases(
+    server: MySQLServer,
+    callback: (d: MySQLDatabase) => void,
+  ): Promise<void> {
+    const client = ((await this.getAuthenticatedClient(
+      MySQLManagementClient as any,
+    )) as unknown) as MySQLManagementClient;
+    const resourceGroup = resourceGroupName(server.id, true)!;
+    const serverName = server.name!;
+
+    return this.iterateScopedResources(
+      {
+        list: async () => {
+          return client.databases.listByServer(resourceGroup, serverName);
+        },
+      } as any,
+      callback,
+    );
+  }
+
+  //// Private Functions ////
 
   private async getAuthenticatedClient<T extends AzureServiceClient>(
     ctor: ClientConstructor<T>,
@@ -189,7 +244,6 @@ export default class ResourceManagerClient {
    * @param rmModule a module that supports listAll() and listAllNext()
    * @param callback a function to receive each resource throughout pagination
    */
-
   private async iterateAllResources<T, L extends ResourceListResponse<T>>(
     rmModule: ResourceManagementModule,
     callback: (r: T) => void | Promise<void>,

1a493706adc32d39d57760d284ce2789263702ea
Adam Williams <adam.williams@lifeomic.com>
2020-04-13 12:44:43 -0400

    Provide optional limits on Terraformed resources

---
+++ terraform/vars.tf
@@ -0,0 +1,24 @@
+variable "azurerm_compute_virtual_machines" {
+  type    = number
+  default = 0
+}
+
+variable "azurerm_storage_sql_servers" {
+  type    = number
+  default = 0
+}
+
+variable "azurerm_storage_sql_databases" {
+  type    = number
+  default = 0
+}
+
+variable "azurerm_storage_mysql_servers" {
+  type    = number
+  default = 0
+}
+
+variable "azurerm_storage_mysql_databases" {
+  type    = number
+  default = 0
+}

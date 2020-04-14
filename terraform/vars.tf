5bf59f22d88c3a9b0be0f0a9f0bcedf2e3efa3dd
Adam Williams <adam.williams@lifeomic.com>
2020-04-14 13:04:37 -0400

    Terraform Cosmos DB SQL database

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -3,6 +3,11 @@ variable "azurerm_compute_virtual_machines" {
   default = 0
 }
 
+variable "azurerm_cosmosdb_sql_databases" {
+  type    = number
+  default = 0
+}
+
 variable "azurerm_storage_sql_servers" {
   type    = number
   default = 0

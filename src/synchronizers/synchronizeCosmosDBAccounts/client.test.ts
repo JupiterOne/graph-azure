be797e869cf9e663cd572422df945e115ce05804
Adam Williams <adam.williams@lifeomic.com>
2020-04-15 11:52:43 -0400

    Add capture of cosmosdb sql database listing

--- src/synchronizers/synchronizeCosmosDBAccounts/client.test.ts
+++ src/synchronizers/synchronizeCosmosDBAccounts/client.test.ts
@@ -1,10 +1,13 @@
+import {
+  DatabaseAccountGetResults,
+  SqlDatabaseResource,
+} from "@azure/arm-cosmosdb/esm/models";
+import { createTestLogger } from "@jupiterone/jupiter-managed-integration-sdk";
 import { Polly } from "@pollyjs/core";
+
 import polly from "../../../test/helpers/polly";
 import config from "../../../test/integrationInstanceConfig";
-
 import { CosmosDBClient } from "./client";
-import { createTestLogger } from "@jupiterone/jupiter-managed-integration-sdk";
-import { DatabaseAccountGetResults } from "@azure/arm-cosmosdb/esm/models";
 
 let p: Polly;
 
@@ -30,6 +33,33 @@ describe("iterateAccounts", () => {
         tags: expect.objectContaining({
           environment: "j1dev",
         }),
+      }),
+    ]);
+  });
+});
+
+describe("iterateSQLDatabases", () => {
+  test("all", async () => {
+    p = polly(__dirname, "iterateSQLDatabases", { recordFailedRequests: true });
+
+    const client = new CosmosDBClient(config, createTestLogger(), true);
+
+    const dbAccount: DatabaseAccountGetResults = {
+      name: "j1dev",
+      id:
+        "/subscriptions/dccea45f-7035-4a17-8731-1fd46aaa74a0/resourceGroups/j1dev/providers/Microsoft.DocumentDB/databaseAccounts/j1dev",
+    };
+
+    const resources: SqlDatabaseResource[] = [];
+    await client.iterateSQLDatabases(dbAccount, (e) => {
+      resources.push(e);
+    });
+
+    expect(resources).toEqual([
+      expect.objectContaining({
+        id: expect.any(String),
+        name: "j1dev",
+        type: "Microsoft.DocumentDB/databaseAccounts/sqlDatabases",
       }),
     ]);
   });

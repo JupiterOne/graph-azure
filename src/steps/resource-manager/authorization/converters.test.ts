420083b8389ec5f1d0cb405738f39541d30db22e
Nick Dowmon <nick.dowmon@lifeomic.com>
2020-07-22 17:28:55 -0400

    Add authorization steps

---
+++ src/steps/resource-manager/authorization/converters.test.ts
@@ -0,0 +1,52 @@
+import { convertProperties } from '@jupiterone/integration-sdk-core';
+
+import { createAzureWebLinker } from '../../../azure';
+import { createRoleDefinitionEntity } from './converters';
+import { RoleDefinition } from '@azure/arm-authorization/esm/models';
+
+const webLinker = createAzureWebLinker('something.onmicrosoft.com');
+
+describe('createRoleDefinitionEntity', () => {
+  test('properties transferred', () => {
+    const data: RoleDefinition = {
+      id:
+        '/providers/Microsoft.Authorization/roleDefinitions/8e3af657-a8ff-443c-a75c-2fe8c4bcb635',
+      name: '8e3af657-a8ff-443c-a75c-2fe8c4bcb635',
+      type: 'Microsoft.Authorization/roleDefinitions',
+      roleName: 'Owner',
+      description: 'Lets you manage everything, including access to resources.',
+      roleType: 'BuiltInRole',
+      permissions: [
+        {
+          actions: ['*'],
+          notActions: [],
+          dataActions: [],
+          notDataActions: [],
+        },
+      ],
+      assignableScopes: ['/'],
+    };
+
+    expect(createRoleDefinitionEntity(webLinker, data)).toEqual({
+      ...convertProperties(data),
+      _key:
+        '/providers/Microsoft.Authorization/roleDefinitions/8e3af657-a8ff-443c-a75c-2fe8c4bcb635',
+      _type: 'azure_role_definition',
+      _class: ['AccessRole', 'AccessPolicy'],
+      _rawData: [{ name: 'default', rawData: data }],
+      displayName: 'Owner',
+      type: 'Microsoft.Authorization/roleDefinitions',
+      webLink: webLinker.portalResourceUrl(
+        '/providers/Microsoft.Authorization/roleDefinitions/8e3af657-a8ff-443c-a75c-2fe8c4bcb635',
+      ),
+      rules: [
+        {
+          actions: ['*'],
+          dataActions: [],
+          notActions: [],
+          notDataActions: [],
+        },
+      ],
+    });
+  });
+});
---
+++ src/steps/resource-manager/authorization/converters.ts
@@ -0,0 +1,26 @@
+import { RoleDefinition } from '@azure/arm-authorization/esm/models';
+import { convertProperties, Entity } from '@jupiterone/integration-sdk-core';
+
+import { AzureWebLinker } from '../../../azure';
+import {
+  ROLE_DEFINITION_ENTITY_CLASS,
+  ROLE_DEFINITION_ENTITY_TYPE,
+} from './constants';
+
+export function createRoleDefinitionEntity(
+  webLinker: AzureWebLinker,
+  data: RoleDefinition,
+): Entity {
+  const entity = {
+    ...convertProperties(data),
+    _key: data.id as string,
+    _type: ROLE_DEFINITION_ENTITY_TYPE,
+    _class: ROLE_DEFINITION_ENTITY_CLASS,
+    _rawData: [{ name: 'default', rawData: data }],
+    displayName: data.roleName,
+    description: data.description,
+    rules: data.permissions,
+    webLink: webLinker.portalResourceUrl(data.id),
+  };
+  return entity;
+}

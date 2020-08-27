d9ae1e6695c1af8eae8dbfd3d8dd89bba659dd50
Nick Dowmon <nick.dowmon@jupiterone.com>
2020-08-27 12:49:46 -0400

    Add tests for findOrBuildResourceEntityFromResourceId

--- src/steps/resource-manager/utils/findOrBuildResourceEntityFromResourceId.test.ts
+++ src/steps/resource-manager/utils/findOrBuildResourceEntityFromResourceId.test.ts
@@ -1,10 +1,11 @@
 import findOrBuildResourceEntityFromResourceId, {
-  getJupiterTypeForResourceId,
+  DEFAULT_RESOURCE_TYPE,
 } from './findOrBuildResourceEntityFromResourceId';
 import { Entity } from '@jupiterone/integration-sdk-core';
 import { createMockStepExecutionContext } from '@jupiterone/integration-sdk-testing';
 import instanceConfig from '../../../../test/integrationInstanceConfig';
 import { IntegrationConfig } from '../../../types';
+import { KEY_VAULT_SERVICE_ENTITY_TYPE } from '../key-vault';
 
 describe('#findOrBuildResourceEntityFromResourceId', () => {
   test('should find resource entity that exists in the job state', async () => {
@@ -12,7 +13,7 @@ describe('#findOrBuildResourceEntityFromResourceId', () => {
       '/subscriptions/subscription-id/resourceGroups/resource-group-id/providers/Microsoft.KeyVault/vaults/key-vault-id';
     const targetEntity: Entity = {
       _class: ['Service'],
-      _type: 'azure_keyvault_service',
+      _type: KEY_VAULT_SERVICE_ENTITY_TYPE,
       _key: id,
     };
 
@@ -28,10 +29,9 @@ describe('#findOrBuildResourceEntityFromResourceId', () => {
     expect(response).toBe(targetEntity);
   });
 
-  test('should build placeholder entity that does not exist in the job state', async () => {
+  test('should build placeholder entity that does not exist in the job state by id', async () => {
     const id =
       '/subscriptions/subscription-id/resourceGroups/resource-group-id/providers/Microsoft.KeyVault/vaults/key-vault-id';
-    const entityType = getJupiterTypeForResourceId(id);
 
     const context = createMockStepExecutionContext<IntegrationConfig>({
       instanceConfig,
@@ -43,7 +43,65 @@ describe('#findOrBuildResourceEntityFromResourceId', () => {
     });
 
     expect(response).toEqual({
-      _type: entityType,
+      _type: KEY_VAULT_SERVICE_ENTITY_TYPE,
+      _key: id,
+    });
+  });
+
+  test('should return default _type placeholder entity if resourceId does not match', async () => {
+    const id = 'some-random-id';
+
+    const context = createMockStepExecutionContext<IntegrationConfig>({
+      instanceConfig,
+      entities: [],
+    });
+
+    const response = await findOrBuildResourceEntityFromResourceId(context, {
+      resourceId: id,
+    });
+
+    expect(response).toEqual({
+      _type: DEFAULT_RESOURCE_TYPE,
+      _key: id,
+    });
+  });
+
+  test('should build placeholder entity that does not exist in the job state by type', async () => {
+    const id = 'some-random-id';
+    const type = 'Microsoft.KeyVault/vault';
+
+    const context = createMockStepExecutionContext<IntegrationConfig>({
+      instanceConfig,
+      entities: [],
+    });
+
+    const response = await findOrBuildResourceEntityFromResourceId(context, {
+      resourceId: id,
+      type,
+    });
+
+    expect(response).toEqual({
+      _type: KEY_VAULT_SERVICE_ENTITY_TYPE,
+      _key: id,
+    });
+  });
+
+  test('should return default _type placeholder entity if _type does not match', async () => {
+    const id = 'some-random-id';
+    const type = 'some-random-type';
+
+    const context = createMockStepExecutionContext<IntegrationConfig>({
+      instanceConfig,
+      entities: [],
+    });
+
+    const response = await findOrBuildResourceEntityFromResourceId(context, {
+      resourceId: id,
+      type,
+    });
+
+    expect(response).toEqual({
+      _type: DEFAULT_RESOURCE_TYPE,
       _key: id,
     });
   });

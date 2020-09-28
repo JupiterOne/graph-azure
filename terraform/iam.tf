e0eaa7735a28fdd2f7b8536f6d67b4bbdd24a334
Keionne Derousselle <knnderousselle@gmail.com>
2020-09-28 09:51:01 -0600

    fixing the terraform provider version and using new prop for role definitions

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,6 +1,6 @@
 resource "azurerm_role_definition" "j1dev_subscription" {
-  name              = "j1dev-subscription"
-  scope             = data.azurerm_subscription.j1dev.id
+  name  = "j1dev-subscription"
+  scope = data.azurerm_subscription.j1dev.id
   assignable_scopes = [
     data.azurerm_subscription.j1dev.id,
   ]
@@ -12,55 +12,55 @@ resource "azurerm_role_definition" "j1dev_subscription" {
 resource "azurerm_role_assignment" "j1dev" {
   name               = "10000000-0000-0000-0000-000000000000"
   scope              = data.azurerm_subscription.j1dev.id
-  role_definition_id = split("|", azurerm_role_definition.j1dev_subscription.id)[0]
+  role_definition_id = azurerm_role_definition.j1dev_subscription.role_definition_resource_id
   principal_id       = data.azurerm_client_config.current.object_id
 }
 
 resource "azurerm_role_assignment" "j1dev-resource-group" {
   name               = "20000000-0000-0000-0000-000000000000"
   scope              = azurerm_resource_group.j1dev.id
-  role_definition_id = split("|", azurerm_role_definition.j1dev_subscription.id)[0]
+  role_definition_id = azurerm_role_definition.j1dev_subscription.role_definition_resource_id
   principal_id       = data.azurerm_client_config.current.object_id
 }
 
 resource "azurerm_role_assignment" "j1dev-resource-keyvault" {
   name               = "30000000-0000-0000-0000-000000000000"
   scope              = azurerm_key_vault.j1dev.id
-  role_definition_id = split("|", azurerm_role_definition.j1dev_subscription.id)[0]
+  role_definition_id = azurerm_role_definition.j1dev_subscription.role_definition_resource_id
   principal_id       = data.azurerm_client_config.current.object_id
 }
 
 resource "azurerm_role_assignment" "j1dev-resource-network-interface" {
   name               = "40000000-0000-0000-0000-000000000000"
   scope              = azurerm_network_interface.j1dev.id
-  role_definition_id = split("|", azurerm_role_definition.j1dev_subscription.id)[0]
+  role_definition_id = azurerm_role_definition.j1dev_subscription.role_definition_resource_id
   principal_id       = data.azurerm_client_config.current.object_id
 }
 
 resource "azurerm_role_assignment" "j1dev-resource-security-group" {
   name               = "50000000-0000-0000-0000-000000000000"
   scope              = azurerm_network_security_group.j1dev.id
-  role_definition_id = split("|", azurerm_role_definition.j1dev_subscription.id)[0]
+  role_definition_id = azurerm_role_definition.j1dev_subscription.role_definition_resource_id
   principal_id       = data.azurerm_client_config.current.object_id
 }
 
 resource "azurerm_role_assignment" "j1dev-resource-public-ip" {
   name               = "60000000-0000-0000-0000-000000000000"
   scope              = azurerm_public_ip.j1dev.id
-  role_definition_id = split("|", azurerm_role_definition.j1dev_subscription.id)[0]
+  role_definition_id = azurerm_role_definition.j1dev_subscription.role_definition_resource_id
   principal_id       = data.azurerm_client_config.current.object_id
 }
 
 resource "azurerm_role_assignment" "j1dev-resource-virtual-network" {
   name               = "70000000-0000-0000-0000-000000000000"
   scope              = azurerm_virtual_network.j1dev.id
-  role_definition_id = split("|", azurerm_role_definition.j1dev_subscription.id)[0]
+  role_definition_id = azurerm_role_definition.j1dev_subscription.role_definition_resource_id
   principal_id       = data.azurerm_client_config.current.object_id
 }
 
 resource "azurerm_role_assignment" "j1dev-resource-cosmosdb-account" {
   name               = "80000000-0000-0000-0000-000000000000"
   scope              = azurerm_cosmosdb_account.j1dev.id
-  role_definition_id = split("|", azurerm_role_definition.j1dev_subscription.id)[0]
+  role_definition_id = azurerm_role_definition.j1dev_subscription.role_definition_resource_id
   principal_id       = data.azurerm_client_config.current.object_id
 }

c018602ae9f2042685728d63c18650fb32fd71f3
Keionne Derousselle <knnderousselle@gmail.com>
2020-10-27 16:31:34 -0600

    adding constants

---
+++ src/steps/resource-manager/policy/constants.ts
@@ -0,0 +1,36 @@
+import {
+  generateRelationshipType,
+  RelationshipClass,
+} from '@jupiterone/integration-sdk-core';
+import { SUBSCRIPTION_ENTITY_METADATA } from '../subscriptions';
+import { createResourceGroupResourceRelationshipMetadata } from '../utils/createResourceGroupResourceRelationship';
+
+// TODO: Add a Step for getting Policy Definitions
+// TODO: Add a Step for getting Policy Set Definitions
+
+export const STEP_RM_POLICY_ASSIGNMENTS = 'rm-policy-assignments';
+
+export const PolicyEntities = {
+  POLICY_ASSIGNMENT: {
+    _type: 'azure_policy_assignment',
+    _class: ['ControlPolicy'],
+    resourceName: '[RM] Policy Assignment',
+  },
+};
+
+export const PolicyRelationships = {
+  RESOURCE_GROUP_HAS_POLICY_ASSIGNMENT: createResourceGroupResourceRelationshipMetadata(
+    PolicyEntities.POLICY_ASSIGNMENT._type,
+  ),
+
+  SUBSCRIPTION_HAS_POLICY_ASSIGNMENT: {
+    _type: generateRelationshipType(
+      RelationshipClass.HAS,
+      SUBSCRIPTION_ENTITY_METADATA._type,
+      PolicyEntities.POLICY_ASSIGNMENT._type,
+    ),
+    sourceType: SUBSCRIPTION_ENTITY_METADATA._type,
+    _class: RelationshipClass.HAS,
+    targetType: PolicyEntities.POLICY_ASSIGNMENT._type,
+  },
+};

d94e055130eb9daca61984efeb7fd0b036d397b5
Keionne Derousselle <knnderousselle@gmail.com>
2020-12-10 11:43:04 -0700

    refactoring the diagnostic settings helper

--- src/utils/diagnosticSettings.ts
+++ src/utils/diagnosticSettings.ts
@@ -1,34 +1,22 @@
 import {
   createDirectRelationship,
   Entity,
+  ExplicitRelationship,
   JobState,
   RelationshipClass,
 } from '@jupiterone/integration-sdk-core';
-import { AzureWebLinker, createAzureWebLinker } from '../azure';
+import { createAzureWebLinker } from '../azure';
 import { IntegrationStepContext } from '../types';
 import { ACCOUNT_ENTITY_TYPE } from '../steps/active-directory/constants';
 import { MonitorClient } from '../steps/resource-manager/monitor/client';
-import {
-  MonitorEntities,
-  MonitorRelationships,
-} from '../steps/resource-manager/monitor/constants';
+import { MonitorRelationships } from '../steps/resource-manager/monitor/constants';
 import {
   createDiagnosticLogSettingEntity,
   createDiagnosticMetricSettingEntity,
 } from '../steps/resource-manager/monitor/converters';
-import {
-  DiagnosticSettingsResource,
-  LogSettings,
-  MetricSettings,
-} from '@azure/arm-monitor/esm/models';
+import { DiagnosticSettingsResource } from '@azure/arm-monitor/esm/models';
 import { STORAGE_ACCOUNT_ENTITY_METADATA } from '../steps/resource-manager/storage/constants';
 import { ANY_SCOPE } from '../steps/resource-manager/constants';
-
-type CreateDiagnosticSettingEntity = (
-  webLinker: AzureWebLinker,
-  diagnosticSettings: DiagnosticSettingsResource,
-  setting: LogSettings | MetricSettings,
-) => Entity;
 
 type StorageLocation = {
   getStorageId: (
@@ -37,31 +25,8 @@ type StorageLocation = {
   storageType: string;
 };
 
-type DIAGNOSTIC_SETTING_INFO = {
-  createEntity: CreateDiagnosticSettingEntity;
-  relationshipType: string;
-};
-
-const DIAGNOSTIC_SETTING_INFO_MAP: {
-  [key: string]: DIAGNOSTIC_SETTING_INFO;
-} = {
-  [MonitorEntities.DIAGNOSTIC_LOG_SETTING._type]: {
-    createEntity: createDiagnosticLogSettingEntity,
-    relationshipType:
-      MonitorRelationships.AZURE_RESOURCE_HAS_MONITOR_DIAGNOSTIC_LOG_SETTING
-        ._type,
-  },
-
-  [MonitorEntities.DIAGNOSTIC_METRIC_SETTING._type]: {
-    createEntity: createDiagnosticMetricSettingEntity,
-    relationshipType:
-      MonitorRelationships.AZURE_RESOURCE_HAS_MONITOR_DIAGNOSTIC_METRIC_SETTING
-        ._type,
-  },
-};
-
 // TODO: Add the other types of storage when we ingest them. (Azure Log Analytics, Azure Event Hub)
-const STORAGE_LOCATION_INFO: StorageLocation[] = [
+const STORAGE_LOCATIONS: StorageLocation[] = [
   {
     getStorageId: (diagnosticSettings: DiagnosticSettingsResource) =>
       diagnosticSettings.storageAccountId,
@@ -69,56 +34,70 @@ const STORAGE_LOCATION_INFO: StorageLocation[] = [
   },
 ];
 
-async function createEntitiesAndRelationships(
-  jobState: JobState,
-  webLinker: AzureWebLinker,
-  diagnosticSettings: DiagnosticSettingsResource,
-  setting: LogSettings | MetricSettings,
-  entityType: string,
+/**
+ * Creates a direct/explicit relationship between an Azure Scope and an Azure Diagnostic Log Setting or Azure Diagnostic Metric Setting
+ * @param resourceEntity The Azure scope that has an Azure Diagnostic Log or Metric Setting
+ * @param settingEntity The Azure Diagnostic Log or Metric Setting Entity
+ * @param relationshipType The type of relationship between the Azure Scope and Azure Diagnostic Log or Metric Setting (e.g azure_resource_has_diagnostic_log_setting)
+ */
+function createResourceHasSettingRelationship(
   resourceEntity: Entity,
-): Promise<void> {
-  if (!DIAGNOSTIC_SETTING_INFO_MAP[entityType]) return;
+  settingEntity: Entity,
+  relationshipType: string,
+): ExplicitRelationship {
+  return createDirectRelationship({
+    _class: RelationshipClass.HAS,
+    fromType: ANY_SCOPE,
+    fromKey: resourceEntity._key,
+    toType: settingEntity._type,
+    toKey: settingEntity._key,
+    properties: {
+      _type: relationshipType,
+    },
+  });
+}
 
-  const { createEntity, relationshipType } = DIAGNOSTIC_SETTING_INFO_MAP[
-    entityType
-  ];
+/**
+ * Creates direct/explicit relationships between an Azure Diagnostic Log Setting or Azure Diagnostic Metric Setting and the Azure Storage Resource(s) (Event Hub, Log Analytics, or Storage Account) that it uses.
+ * @param diagnosticSettings The Azure Diagnostic Settings that contains the storage locations of the log and metrics
+ * @param settingEntity The Azure Diagnostic Log or Metric Setting Entity
+ */
+function createSettingUsesStorageRelationships(
+  diagnosticSettings: DiagnosticSettingsResource,
+  settingEntity: Entity,
+): ExplicitRelationship[] {
+  return STORAGE_LOCATIONS.reduce<ExplicitRelationship[]>(
+    (relationships, storageLocation) => {
+      const { getStorageId, storageType } = storageLocation;
+      const storageId = getStorageId(diagnosticSettings);
 
-  // Create Azure Diagnostic Log Setting or Azure Diagnostic Metric Setting entity
-  const settingEntity = await jobState.addEntity(
-    createEntity(webLinker, diagnosticSettings, setting),
+      if (storageId) {
+        relationships.push(
+          createDirectRelationship({
+            _class: RelationshipClass.USES,
+            fromType: settingEntity._type,
+            fromKey: settingEntity._key,
+            toType: storageType,
+            toKey: storageId,
+          }),
+        );
+      }
+
+      return relationships;
+    },
+    [],
   );
+}
 
-  // Create Azure Resource has Azure Diagnostic Log Setting or Azure Diagnostic Metric Setting relationship
-  await jobState.addRelationship(
-    createDirectRelationship({
-      _class: RelationshipClass.HAS,
-      fromType: ANY_SCOPE,
-      fromKey: resourceEntity._key,
-      toType: settingEntity._type,
-      toKey: settingEntity._key,
-      properties: {
-        _type: relationshipType,
-      },
-    }),
-  );
-
-  // Create Azure Diagnostic Log Setting or Azure Diagnostic Metric Setting uses Azure Storage Resource (Event Hub, Log Analytics, or Storage Account)
-  for (const storageLocation of STORAGE_LOCATION_INFO) {
-    const { getStorageId, storageType } = storageLocation;
-
-    const storageId = getStorageId(diagnosticSettings);
-    if (!storageId) continue;
-
-    await jobState.addRelationship(
-      createDirectRelationship({
-        _class: RelationshipClass.USES,
-        fromType: settingEntity._type,
-        fromKey: settingEntity._key,
-        toType: storageType,
-        toKey: storageId,
-      }),
-    );
-  }
+function persistDiagnosticSettingsGraphObjects(
+  jobState: JobState,
+  settingEntity: Entity,
+  relationships: ExplicitRelationship[],
+) {
+  return Promise.all([
+    jobState.addEntity(settingEntity),
+    jobState.addRelationships(relationships),
+  ]);
 }
 
 export async function createDiagnosticSettingsEntitiesAndRelationshipsForResource(
@@ -139,30 +118,60 @@ export async function createDiagnosticSettingsEntitiesAndRelationshipsForResourc
 
       await Promise.all([
         ...(logs
-          ? logs.map(
-              async (log) =>
-                await createEntitiesAndRelationships(
-                  jobState,
-                  webLinker,
-                  diagnosticSettings,
-                  log,
-                  MonitorEntities.DIAGNOSTIC_LOG_SETTING._type,
-                  resourceEntity,
-                ),
-            )
+          ? logs.map(async (log) => {
+              const logSettingEntity = createDiagnosticLogSettingEntity(
+                webLinker,
+                diagnosticSettings,
+                log,
+              );
+              const resourceHasLogSettingRelationship = createResourceHasSettingRelationship(
+                resourceEntity,
+                logSettingEntity,
+                MonitorRelationships
+                  .AZURE_RESOURCE_HAS_MONITOR_DIAGNOSTIC_LOG_SETTING._type,
+              );
+              const logSettingUsesStorageRelationships = createSettingUsesStorageRelationships(
+                diagnosticSettings,
+                logSettingEntity,
+              );
+
+              await persistDiagnosticSettingsGraphObjects(
+                jobState,
+                logSettingEntity,
+                [
+                  resourceHasLogSettingRelationship,
+                  ...(logSettingUsesStorageRelationships || []),
+                ],
+              );
+            })
           : []),
         ...(metrics
-          ? metrics.map(
-              async (metric) =>
-                await createEntitiesAndRelationships(
-                  jobState,
-                  webLinker,
-                  diagnosticSettings,
-                  metric,
-                  MonitorEntities.DIAGNOSTIC_METRIC_SETTING._type,
-                  resourceEntity,
-                ),
-            )
+          ? metrics.map(async (metric) => {
+              const metricSettingEntity = createDiagnosticMetricSettingEntity(
+                webLinker,
+                diagnosticSettings,
+                metric,
+              );
+              const resourceHasMetricSettingRelationship = createResourceHasSettingRelationship(
+                resourceEntity,
+                metricSettingEntity,
+                MonitorRelationships
+                  .AZURE_RESOURCE_HAS_MONITOR_DIAGNOSTIC_METRIC_SETTING._type,
+              );
+              const metricSettingUsesStorageRelationships = createSettingUsesStorageRelationships(
+                diagnosticSettings,
+                metricSettingEntity,
+              );
+
+              await persistDiagnosticSettingsGraphObjects(
+                jobState,
+                metricSettingEntity,
+                [
+                  resourceHasMetricSettingRelationship,
+                  ...(metricSettingUsesStorageRelationships || []),
+                ],
+              );
+            })
           : []),
       ]);
     },

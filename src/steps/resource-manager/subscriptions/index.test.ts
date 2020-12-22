f25d8223c986f9a0dee2b27edaa111cceeb178f5
Keionne Derousselle <knnderousselle@gmail.com>
2020-12-22 15:33:25 -0700

    adding diagnostic settings for an azure subscription (activity logs)

--- src/steps/resource-manager/subscriptions/index.test.ts
+++ src/steps/resource-manager/subscriptions/index.test.ts
@@ -1,58 +1,144 @@
 import { fetchSubscriptions } from '.';
-import { Recording } from '@jupiterone/integration-sdk-testing';
+import {
+  MockIntegrationStepExecutionContext,
+  Recording,
+} from '@jupiterone/integration-sdk-testing';
 import { IntegrationConfig } from '../../../types';
 import { setupAzureRecording } from '../../../../test/helpers/recording';
 import { createMockAzureStepExecutionContext } from '../../../../test/createMockAzureStepExecutionContext';
-import { ACCOUNT_ENTITY_TYPE } from '../../active-directory';
+import { ACCOUNT_ENTITY_TYPE } from '../../active-directory/constants';
+import { MonitorEntities } from '../monitor/constants';
+import { SUBSCRIPTION_ENTITY_METADATA } from './constants';
 let recording: Recording;
 
-afterEach(async () => {
+afterAll(async () => {
   if (recording) {
     await recording.stop();
   }
 });
 
-test('step - subscriptions', async () => {
-  const instanceConfig: IntegrationConfig = {
-    clientId: process.env.CLIENT_ID || 'clientId',
-    clientSecret: process.env.CLIENT_SECRET || 'clientSecret',
-    directoryId: '992d7bbe-b367-459c-a10f-cf3fd16103ab',
-    subscriptionId: 'd3803fd6-2ba4-4286-80aa-f3d613ad59a7',
-  };
+describe('step - subscriptions', () => {
+  let context: MockIntegrationStepExecutionContext<IntegrationConfig>;
+  let instanceConfig: IntegrationConfig;
 
-  recording = setupAzureRecording({
-    directory: __dirname,
-    name: 'resource-manager-step-resource-groups',
-  });
+  beforeAll(async () => {
+    instanceConfig = {
+      clientId: process.env.CLIENT_ID || 'clientId',
+      clientSecret: process.env.CLIENT_SECRET || 'clientSecret',
+      directoryId: 'bcd90474-9b62-4040-9d7b-8af257b1427d',
+      subscriptionId: '40474ebe-55a2-4071-8fa8-b610acdd8e56',
+      developerId: 'keionned',
+    };
 
-  const context = createMockAzureStepExecutionContext({
-    instanceConfig,
-    setData: {
-      [ACCOUNT_ENTITY_TYPE]: { defaultDomain: 'www.fake-domain.com' },
-    },
-  });
+    recording = setupAzureRecording({
+      directory: __dirname,
+      name: 'resource-manager-step-resource-groups',
+    });
 
-  await fetchSubscriptions(context);
-
-  expect(context.jobState.collectedEntities.length).toBeGreaterThan(0);
-  expect(context.jobState.collectedEntities).toMatchGraphObjectSchema({
-    _class: 'Account',
-    schema: {
-      additionalProperties: false,
-      properties: {
-        _type: { const: 'azure_subscription' },
-        _key: { type: 'string' },
-        _class: { type: 'array', items: { const: 'Group' } },
-        id: { type: 'string' },
-        name: { type: 'string' },
-        displayName: { type: 'string' },
-        subscriptionId: { type: 'string' },
-        state: { type: 'string' },
-        authorizationSource: { type: 'string' },
-        webLink: { type: 'string' },
-        _rawData: { type: 'array', items: { type: 'object' } },
+    context = createMockAzureStepExecutionContext({
+      instanceConfig,
+      setData: {
+        [ACCOUNT_ENTITY_TYPE]: { defaultDomain: 'www.fake-domain.com' },
       },
-    },
+    });
+
+    await fetchSubscriptions(context);
   });
-  expect(context.jobState.collectedRelationships.length).toBe(0);
+
+  it('should collect a Subscription entity', () => {
+    const { collectedEntities } = context.jobState;
+
+    expect(collectedEntities).toContainEqual(
+      expect.objectContaining({
+        _class: SUBSCRIPTION_ENTITY_METADATA._class,
+        _type: SUBSCRIPTION_ENTITY_METADATA._type,
+        _key: `/subscriptions/${instanceConfig.subscriptionId}`,
+        id: `/subscriptions/${instanceConfig.subscriptionId}`,
+        name: expect.any(String),
+        displayName: expect.any(String),
+        subscriptionId: instanceConfig.subscriptionId,
+        state: 'Enabled',
+        authorizationSource: 'RoleBased',
+        webLink: `https://portal.azure.com/#@www.fake-domain.com/resource/subscriptions/${instanceConfig.subscriptionId}`,
+      }),
+    );
+  });
+
+  it('should collect the first Diagnostic Log Setting entity', () => {
+    const { collectedEntities } = context.jobState;
+
+    expect(collectedEntities).toContainEqual(
+      expect.objectContaining({
+        _class: MonitorEntities.DIAGNOSTIC_LOG_SETTING._class,
+        _key: `/subscriptions/${instanceConfig.subscriptionId}/providers/microsoft.insights/diagnosticSettings/j1dev_sub_diag_set/logs/Policy/true/undefined/undefined`,
+        _type: MonitorEntities.DIAGNOSTIC_LOG_SETTING._type,
+        category: 'Policy',
+        displayName: 'j1dev_sub_diag_set',
+        enabled: true,
+        eventHubAuthorizationRuleId: undefined,
+        eventHubName: undefined,
+        id: `/subscriptions/${instanceConfig.subscriptionId}/providers/microsoft.insights/diagnosticSettings/j1dev_sub_diag_set/logs/Policy/true/undefined/undefined`,
+        logAnalyticsDestinationType: undefined,
+        name: 'j1dev_sub_diag_set',
+        'retentionPolicy.days': undefined,
+        'retentionPolicy.enabled': undefined,
+        serviceBusRuleId: undefined,
+        storageAccountId: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Storage/storageAccounts/${instanceConfig.developerId}j1dev`,
+        webLink: `https://portal.azure.com/#@www.fake-domain.com/resource/subscriptions/${instanceConfig.subscriptionId}/providers/microsoft.insights/diagnosticSettings/j1dev_sub_diag_set`,
+        workspaceId: undefined,
+      }),
+    );
+  });
+
+  it('should collect the second Diagnostic Log Setting entity', () => {
+    const { collectedEntities } = context.jobState;
+
+    expect(collectedEntities).toContainEqual(
+      expect.objectContaining({
+        _class: MonitorEntities.DIAGNOSTIC_LOG_SETTING._class,
+        _key: `/subscriptions/${instanceConfig.subscriptionId}/providers/microsoft.insights/diagnosticSettings/j1dev_sub_diag_set/logs/Security/true/undefined/undefined`,
+        _type: MonitorEntities.DIAGNOSTIC_LOG_SETTING._type,
+        category: 'Security',
+        displayName: 'j1dev_sub_diag_set',
+        enabled: true,
+        eventHubAuthorizationRuleId: undefined,
+        eventHubName: undefined,
+        id: `/subscriptions/${instanceConfig.subscriptionId}/providers/microsoft.insights/diagnosticSettings/j1dev_sub_diag_set/logs/Security/true/undefined/undefined`,
+        logAnalyticsDestinationType: undefined,
+        name: 'j1dev_sub_diag_set',
+        'retentionPolicy.days': undefined,
+        'retentionPolicy.enabled': undefined,
+        serviceBusRuleId: undefined,
+        storageAccountId: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Storage/storageAccounts/${instanceConfig.developerId}j1dev`,
+        webLink: `https://portal.azure.com/#@www.fake-domain.com/resource/subscriptions/${instanceConfig.subscriptionId}/providers/microsoft.insights/diagnosticSettings/j1dev_sub_diag_set`,
+        workspaceId: undefined,
+      }),
+    );
+  });
+
+  it('should collect the first Diagnostic Log Setting uses Storage Account relationship', () => {
+    const { collectedRelationships } = context.jobState;
+
+    expect(collectedRelationships).toContainEqual({
+      _class: 'HAS',
+      _fromEntityKey: `/subscriptions/${instanceConfig.subscriptionId}`,
+      _key: `/subscriptions/${instanceConfig.subscriptionId}|has|/subscriptions/${instanceConfig.subscriptionId}/providers/microsoft.insights/diagnosticSettings/j1dev_sub_diag_set/logs/Policy/true/undefined/undefined`,
+      _toEntityKey: `/subscriptions/${instanceConfig.subscriptionId}/providers/microsoft.insights/diagnosticSettings/j1dev_sub_diag_set/logs/Policy/true/undefined/undefined`,
+      _type: 'azure_resource_has_diagnostic_log_setting',
+      displayName: 'HAS',
+    });
+  });
+
+  it('should collect the second Diagnostic Log Setting uses Storage Account relationship', () => {
+    const { collectedRelationships } = context.jobState;
+
+    expect(collectedRelationships).toContainEqual({
+      _class: 'HAS',
+      _fromEntityKey: `/subscriptions/${instanceConfig.subscriptionId}`,
+      _key: `/subscriptions/${instanceConfig.subscriptionId}|has|/subscriptions/${instanceConfig.subscriptionId}/providers/microsoft.insights/diagnosticSettings/j1dev_sub_diag_set/logs/Security/true/undefined/undefined`,
+      _toEntityKey: `/subscriptions/${instanceConfig.subscriptionId}/providers/microsoft.insights/diagnosticSettings/j1dev_sub_diag_set/logs/Security/true/undefined/undefined`,
+      _type: 'azure_resource_has_diagnostic_log_setting',
+      displayName: 'HAS',
+    });
+  });
 });

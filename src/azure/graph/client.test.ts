f7e35a26b78c4915e1e4da471dceed7f74cd8d9f
Nick Dowmon <nick.dowmon@jupiterone.com>
2021-01-13 12:23:55 -0500

    Add tests for Graph Client error handling

--- src/azure/graph/client.test.ts
+++ src/azure/graph/client.test.ts
@@ -2,10 +2,12 @@ import {
   createMockIntegrationLogger,
   Recording,
 } from '@jupiterone/integration-sdk-testing';
+import { FetchError } from 'node-fetch';
 
 import config from '../../../test/integrationInstanceConfig';
 import { GraphClient } from './client';
 import { setupAzureRecording } from '../../../test/helpers/recording';
+import { GraphRequest } from '@microsoft/microsoft-graph-client';
 
 class AnyGraphClient extends GraphClient {}
 
@@ -54,4 +56,44 @@ test('fetchOrganization', async () => {
       }),
     ],
   });
+});
+
+test('client.request should retry requests 3 times', async () => {
+  const client = new AnyGraphClient(createMockIntegrationLogger(), config);
+
+  const graphClientError = new Error('Generic Graph client error');
+  (graphClientError as any).statusCode = 400;
+  (graphClientError as any).statusText = 'Server Error';
+  const mockGet = jest.fn().mockRejectedValue(graphClientError);
+
+  const mockGraphRequest: GraphRequest = ({
+    get: mockGet,
+    buildFullUrl: () => 'https://hostname/endpoint',
+  } as unknown) as GraphRequest;
+
+  await expect(client.request(mockGraphRequest)).rejects.toThrow(
+    'Provider API failed at https://hostname/endpoint: 400 Server Error',
+  );
+
+  expect(mockGet).toHaveBeenCalledTimes(3);
+});
+
+test('client.request should expose node-fetch error codes', async () => {
+  const client = new AnyGraphClient(createMockIntegrationLogger(), config);
+
+  const systemError = new Error('system error');
+  (systemError as any).code = 'ECONNRESET';
+
+  const mockGraphRequest: GraphRequest = ({
+    get: jest
+      .fn()
+      .mockRejectedValue(
+        new FetchError('Error message for system error', 'system', systemError),
+      ),
+    buildFullUrl: () => 'https://hostname/endpoint',
+  } as unknown) as GraphRequest;
+
+  await expect(client.request(mockGraphRequest)).rejects.toThrow(
+    'Provider API failed at https://hostname/endpoint: ECONNRESET Error message for system error',
+  );
 });

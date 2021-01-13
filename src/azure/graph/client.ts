e170cf1b581c2a2dbc6df905e2015b1b43725698
Nick Dowmon <nick.dowmon@jupiterone.com>
2021-01-13 09:50:43 -0500

    Retry azure graph API calls

--- src/azure/graph/client.ts
+++ src/azure/graph/client.ts
@@ -1,14 +1,25 @@
-import { IntegrationLogger } from '@jupiterone/integration-sdk-core';
+import {
+  IntegrationLogger,
+  IntegrationProviderAPIError,
+} from '@jupiterone/integration-sdk-core';
+import { FetchError } from 'node-fetch';
 import {
   AuthenticationProvider,
   Client,
+  GraphRequest,
 } from '@microsoft/microsoft-graph-client';
 import { Organization } from '@microsoft/microsoft-graph-types';
 
 import { IntegrationConfig } from '../../types';
 import authenticate from './authenticate';
+import { retry } from '@lifeomic/attempt';
 
 export type QueryParams = string | { [key: string]: string | number };
+
+interface AzureGraphResponse<TResponseType = any> {
+  value: TResponseType[];
+  ['@odata.nextLink']?: string;
+}
 
 /**
  * Pagination: https://docs.microsoft.com/en-us/graph/paging
@@ -27,13 +38,75 @@ export abstract class GraphClient {
     });
   }
 
-  public async fetchMetadata(): Promise<object> {
-    return this.client.api('/').get();
+  public async fetchMetadata(): Promise<AzureGraphResponse | undefined> {
+    return this.request(this.client.api('/'));
   }
 
   public async fetchOrganization(): Promise<Organization> {
-    const response = await this.client.api('/organization').get();
-    return response.value[0];
+    const response = await this.request<Organization>(
+      this.client.api('/organization'),
+    );
+    return response!.value[0];
+  }
+
+  async retryRequest<TResponseType = any>(
+    graphRequest: GraphRequest,
+  ): Promise<AzureGraphResponse<TResponseType>> {
+    return retry(
+      async () => {
+        return graphRequest.get();
+      },
+      {
+        maxAttempts: 3,
+        delay: 200,
+        handleError: (err, context, options) => {
+          const endpoint = (graphRequest as any).buildFullUrl?.();
+          this.logger.info(
+            {
+              err,
+              endpoint,
+              attemptsRemaining: context.attemptsRemaining,
+            },
+            'Encountered retryable error in Azure Graph API.',
+          );
+        },
+      },
+    );
+  }
+
+  public async request<TResponseType = any>(
+    graphRequest: GraphRequest,
+  ): Promise<AzureGraphResponse<TResponseType> | undefined> {
+    try {
+      const response = await this.retryRequest(graphRequest);
+      return response;
+    } catch (err) {
+      const endpoint = (graphRequest as any).buildFullUrl?.();
+
+      // Fetch errors include the properties code, errno, message, name, stack, type.
+      if (err instanceof FetchError) {
+        throw new IntegrationProviderAPIError({
+          cause: err,
+          endpoint,
+          status: err.code!,
+          statusText: err.message,
+        });
+      }
+
+      if (err.statusCode !== 404) {
+        throw new IntegrationProviderAPIError({
+          cause: err,
+          endpoint,
+          status: err.statusCode,
+          statusText: err.statusText,
+        });
+      }
+
+      this.logger.warn(
+        { err, resourceUrl: endpoint },
+        'Encountered non-fatal error in Azure Graph client. Continuing step execution.',
+      );
+    }
   }
 }
 

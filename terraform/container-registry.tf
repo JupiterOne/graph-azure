71d4112c2c05b4c4e5d21b43983acd97153f5399
Keionne Derousselle <knnderousselle@gmail.com>
2021-02-12 15:00:04 -0700

    adding all possible diagnostic settings for container registry regstries to terraform so that terraform apply is more consistent

--- terraform/container-registry.tf
+++ terraform/container-registry.tf
@@ -20,19 +20,40 @@ resource "azurerm_container_registry" "j1dev" {
   sku                 = "Basic"
 }
 
+data "azurerm_monitor_diagnostic_categories" "j1dev_cont_reg_cat" {
+  count       = local.container_registry_resource_count
+  resource_id = azurerm_container_registry.j1dev[0].id
+}
+
 resource "azurerm_monitor_diagnostic_setting" "j1dev_cont_reg_diag_set" {
   count              = local.container_registry_resource_count
   name               = "j1dev_cont_reg_diag_set"
   target_resource_id = azurerm_container_registry.j1dev[0].id
   storage_account_id = azurerm_storage_account.j1dev.id
 
-  log {
-    category = "ContainerRegistryLoginEvents"
-    enabled  = true
+  dynamic log {
+    for_each = sort(data.azurerm_monitor_diagnostic_categories.j1dev_cont_reg_cat[0].logs)
+    content {
+      category = log.value
+      enabled  = true
 
-    retention_policy {
-      enabled = true
-      days    = 1
+      retention_policy {
+        enabled = true
+        days    = 1
+      }
+    }
+  }
+
+  dynamic metric {
+    for_each = sort(data.azurerm_monitor_diagnostic_categories.j1dev_cont_reg_cat[0].metrics)
+    content {
+      category = metric.value
+      enabled  = true
+
+      retention_policy {
+        enabled = true
+        days    = 1
+      }
     }
   }
 }

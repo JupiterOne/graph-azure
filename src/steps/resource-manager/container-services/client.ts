e16da4e2767850c5c493d392de1a0384f302017f
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-10-05 15:22:57 +0530

    refactor: Use `iterateAllResources` instead of a custom `client`

--- src/steps/resource-manager/container-services/client.ts
+++ src/steps/resource-manager/container-services/client.ts
@@ -1,9 +1,8 @@
-import { Client } from '../../../azure/resource-manager/client';
 import {
-  ContainerServiceClient,
-  ContainerServiceClientContext,
-} from '@azure/arm-containerservice';
-import { IntegrationProviderAPIError } from '@jupiterone/integration-sdk-core';
+  Client,
+  iterateAllResources,
+} from '../../../azure/resource-manager/client';
+import { ContainerServiceClient } from '@azure/arm-containerservice';
 import { ManagedCluster } from '@azure/arm-containerservice/esm/models';
 
 export class ContainerServicesClient extends Client {
@@ -11,29 +10,15 @@ export class ContainerServicesClient extends Client {
     callback: (cluster: ManagedCluster) => void | Promise<void>,
   ): Promise<void> {
     const serviceClient = await this.getAuthenticatedServiceClient(
-      ContainerServiceClientContext,
+      ContainerServiceClient,
     );
-    const client = new ContainerServiceClient(
-      serviceClient.credentials,
-      serviceClient.subscriptionId,
-    );
-    try {
-      const items = await client.managedClusters.list();
-      for (const item of items) {
-        await callback(item);
-      }
-    } catch (err) {
-      /* istanbul ignore else */
-      if (err.statusCode === 404) {
-        this.logger.warn({ err }, 'Clusters not found');
-      } else {
-        throw new IntegrationProviderAPIError({
-          cause: err,
-          endpoint: 'containerServices.kubernetesClusters',
-          status: err.statusCode,
-          statusText: err.statusText,
-        });
-      }
-    }
+
+    return iterateAllResources({
+      logger: this.logger,
+      serviceClient,
+      resourceEndpoint: serviceClient.managedClusters,
+      resourceDescription: 'containerServices.clusters',
+      callback,
+    });
   }
 }

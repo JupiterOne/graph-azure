c9fc85673658820bcac85a0f120a4ac3ad4dcc4d
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-10-07 14:53:58 +0530

    feat: add pagination to `iterateLocks`

--- src/steps/resource-manager/resources/client.ts
+++ src/steps/resource-manager/resources/client.ts
@@ -1,12 +1,11 @@
 import { ResourceManagementClient } from '@azure/arm-resources';
 import { ResourceGroup } from '@azure/arm-resources/esm/models';
-import { Client } from '../../../azure/resource-manager/client';
-import { IntegrationProviderAPIError } from '@jupiterone/integration-sdk-core';
 import {
-  ManagementLockClientContext,
-  ManagementLocks,
-  ManagementLockModels,
-} from '@azure/arm-locks';
+  Client,
+  iterateAllResources,
+} from '../../../azure/resource-manager/client';
+import { IntegrationProviderAPIError } from '@jupiterone/integration-sdk-core';
+import { ManagementLockClient, ManagementLockModels } from '@azure/arm-locks';
 export class ResourcesClient extends Client {
   public async getResourceProvider(resourceProviderNamespace: string) {
     const serviceClient = await this.getAuthenticatedServiceClient(
@@ -49,26 +48,21 @@ export class ResourcesClient extends Client {
     ) => void | Promise<void>,
   ): Promise<void> {
     const serviceClient = await this.getAuthenticatedServiceClient(
-      ManagementLockClientContext,
+      ManagementLockClient,
     );
-    const client = new ManagementLocks(serviceClient);
-    try {
-      const items = await client.listAtResourceGroupLevel(resourceGroupName);
-      for (const item of items) {
-        await callback(item);
-      }
-    } catch (err) {
-      /* istanbul ignore else */
-      if (err.statusCode === 404) {
-        this.logger.warn({ err }, 'Resources not found');
-      } else {
-        throw new IntegrationProviderAPIError({
-          cause: err,
-          endpoint: 'resources.locks',
-          status: err.statusCode,
-          statusText: err.statusText,
-        });
-      }
-    }
+
+    await iterateAllResources({
+      logger: this.logger,
+      serviceClient,
+      resourceEndpoint: {
+        list: () =>
+          serviceClient.managementLocks.listAtResourceGroupLevel(
+            resourceGroupName,
+          ),
+        listNext: serviceClient.managementLocks.listAtResourceGroupLevelNext,
+      },
+      resourceDescription: 'resources.resourceLocks',
+      callback,
+    });
   }
 }

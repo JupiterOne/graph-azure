129daaf532ed2f693859d72808817839ef68b528
Gonzalo Avalos Ribas <gonzaloavalosribas@Gonzalos-MacBook-Pro.local>
2023-01-24 13:03:18 -0300

    Added the precommit hook

--- husky.config.js
+++ husky.config.js
@@ -2,7 +2,7 @@ const { hooks } = require('@jupiterone/integration-sdk-dev-tools/config/husky');
 
 module.exports = {
   hooks: {
-    'pre-commit': `${hooks['pre-commit']} && yarn precommit`,
+    'pre-commit': `${hooks['pre-commit']} && yarn document:permissions && yarn precommit`,
     'pre-push': hooks['pre-push'],
   },
 };

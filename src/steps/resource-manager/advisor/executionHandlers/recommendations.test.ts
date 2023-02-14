a93af3eb67d67ea87b7c6f2d367434310cea2979
Gonzalo Avalos Ribas <gonzaloavalosribas@Gonzalos-MacBook-Pro.local>
2023-02-14 14:39:20 -0300

    Instead of commenting out the tests i just skipped them

--- src/steps/resource-manager/advisor/executionHandlers/recommendations.test.ts
+++ src/steps/resource-manager/advisor/executionHandlers/recommendations.test.ts
@@ -33,40 +33,40 @@ test('rm-advisor-recommendations', async () => {
   expect(stepResults).toMatchStepMetadata(stepTestConfig);
 }, 10_000);
 
-// test('rm-advisor-assessment-recommendation-relationships', async () => {
-//   const stepTestConfig = getStepTestConfigForStep(
-//     AdvisorSteps.ASSESSMENT_RECOMMENDATION_RELATIONSHIP,
-//   );
+test.skip('rm-advisor-assessment-recommendation-relationships', async () => {
+  const stepTestConfig = getStepTestConfigForStep(
+    AdvisorSteps.ASSESSMENT_RECOMMENDATION_RELATIONSHIP,
+  );
 
-//   recording = setupAzureRecording({
-//     name: AdvisorSteps.ASSESSMENT_RECOMMENDATION_RELATIONSHIP,
-//     directory: __dirname,
-//     options: {
-//       matchRequestsBy: getMatchRequestsBy({
-//         config: stepTestConfig.instanceConfig,
-//       }),
-//     },
-//   });
+  recording = setupAzureRecording({
+    name: AdvisorSteps.ASSESSMENT_RECOMMENDATION_RELATIONSHIP,
+    directory: __dirname,
+    options: {
+      matchRequestsBy: getMatchRequestsBy({
+        config: stepTestConfig.instanceConfig,
+      }),
+    },
+  });
 
-//   const stepResults = await executeStepWithDependencies(stepTestConfig);
-//   expect(stepResults).toMatchStepMetadata(stepTestConfig);
-// }, 10_000);
+  const stepResults = await executeStepWithDependencies(stepTestConfig);
+  expect(stepResults).toMatchStepMetadata(stepTestConfig);
+}, 10_000);
 
-// test('rm-advisor-resource-recommendation-relationships', async () => {
-//   const stepTestConfig = getStepTestConfigForStep(
-//     AdvisorSteps.RESOURCE_RECOMMENDATION_RELATIONSHIP,
-//   );
+test.skip('rm-advisor-resource-recommendation-relationships', async () => {
+  const stepTestConfig = getStepTestConfigForStep(
+    AdvisorSteps.RESOURCE_RECOMMENDATION_RELATIONSHIP,
+  );
 
-//   recording = setupAzureRecording({
-//     name: AdvisorSteps.RESOURCE_RECOMMENDATION_RELATIONSHIP,
-//     directory: __dirname,
-//     options: {
-//       matchRequestsBy: getMatchRequestsBy({
-//         config: stepTestConfig.instanceConfig,
-//       }),
-//     },
-//   });
+  recording = setupAzureRecording({
+    name: AdvisorSteps.RESOURCE_RECOMMENDATION_RELATIONSHIP,
+    directory: __dirname,
+    options: {
+      matchRequestsBy: getMatchRequestsBy({
+        config: stepTestConfig.instanceConfig,
+      }),
+    },
+  });
 
-//   const stepResults = await executeStepWithDependencies(stepTestConfig);
-//   expect(stepResults).toMatchStepMetadata(stepTestConfig);
-// }, 10_000);
+  const stepResults = await executeStepWithDependencies(stepTestConfig);
+  expect(stepResults).toMatchStepMetadata(stepTestConfig);
+}, 10_000);

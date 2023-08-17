ef9af5487a56555557ba496b759c385e3277cef6
Gonzalo Avalos Ribas <gonzaloavalosribas@Gonzalos-MacBook-Pro.local>
2023-08-17 13:39:32 -0300

    Added comments

--- globalSetup.ts
+++ globalSetup.ts
@@ -1,3 +1,7 @@
+//Node 18 introduced global fetch. It doesn't behave the same way as cross-fetch.
+//Cross-fetch/pollyfill installs the fetch as global if it doesnt exist.
+//Since on node 18 fetch exists as global, it would prevent pollyfill from installing.
+//Ergo, all recordings would break.
 module.exports = () => {
   const globalAny: any = global;
   globalAny.fetch = undefined;

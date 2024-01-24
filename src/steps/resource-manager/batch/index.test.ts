a70bee3ea48a88f2e6878b10f6c5a54e4afb016e
Gonzalo Avalos Ribas <gonzaloavalosribas@Gonzalos-MacBook-Pro.local>
2024-01-24 12:58:04 -0300

    Batch service

--- src/steps/resource-manager/batch/index.test.ts
+++ src/steps/resource-manager/batch/index.test.ts
@@ -1,270 +1,113 @@
-import { fetchBatchAccounts, fetchBatchCertificates, fetchBatchPools } from '.';
+import { executeStepWithDependencies } from '@jupiterone/integration-sdk-testing';
 import {
-  executeStepWithDependencies,
-  MockIntegrationStepExecutionContext,
   Recording,
-} from '@jupiterone/integration-sdk-testing';
-import { IntegrationConfig } from '../../../types';
-import { setupAzureRecording } from '../../../../test/helpers/recording';
-import { createMockAzureStepExecutionContext } from '../../../../test/createMockAzureStepExecutionContext';
-import { ACCOUNT_ENTITY_TYPE } from '../../active-directory/constants';
-import { BatchEntities, STEP_RM_BATCH_APPLICATION } from './constants';
+  setupAzureRecording,
+  getMatchRequestsBy,
+} from '../../../../test/helpers/recording';
 import { getStepTestConfigForStep } from '../../../../test/integrationInstanceConfig';
+import {
+  STEP_RM_BATCH_ACCOUNT,
+  STEP_RM_BATCH_APPLICATION,
+  STEP_RM_BATCH_CERTIFICATE,
+  STEP_RM_BATCH_POOL,
+} from './constants';
 
 let recording: Recording;
-let context: MockIntegrationStepExecutionContext<IntegrationConfig>;
-let instanceConfig: IntegrationConfig;
 
-describe('step - batch accounts', () => {
-  beforeAll(async () => {
-    recording = setupAzureRecording({
-      directory: __dirname,
-      name: 'resource-manager-step-batch-accounts',
-    });
-
-    instanceConfig = {
-      clientId: process.env.CLIENT_ID || 'clientId',
-      clientSecret: process.env.CLIENT_SECRET || 'clientSecret',
-      directoryId: '4a17becb-fb42-4633-b5c8-5ab66f28d195',
-      subscriptionId: '87f62f44-9dad-4284-a08f-f2fb3d8b528a',
-      developerId: 'keionned',
-    };
-
-    const resourceGroup = {
-      _key: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev`,
-      _type: 'azure_resource_group',
-      _class: ['Group'],
-      name: 'j1dev',
-      location: 'eastus',
-    };
-
-    context = createMockAzureStepExecutionContext({
-      instanceConfig,
-      entities: [resourceGroup],
-      setData: {
-        [ACCOUNT_ENTITY_TYPE]: { defaultDomain: 'www.fake-domain.com' },
-      },
-    });
-
-    await fetchBatchAccounts(context);
-  });
-
-  afterAll(async () => {
-    if (recording) {
-      await recording.stop();
-    }
-  });
-
-  it('should collect an Azure Batch Account entity', () => {
-    const { collectedEntities } = context.jobState;
-
-    expect(collectedEntities).toContainEqual(
-      expect.objectContaining({
-        _class: BatchEntities.BATCH_ACCOUNT._class,
-        _type: BatchEntities.BATCH_ACCOUNT._type,
-        category: ['infrastructure'],
-        accountEndpoint: 'j1devbatchaccount.eastus.batch.azure.com',
-        activeJobAndJobScheduleQuota: 100,
-        dedicatedCoreQuota: 20,
-        lowPriorityCoreQuota: 10,
-        poolAllocationMode: 'BatchService',
-        poolQuota: 20,
-        provisioningState: 'Succeeded',
-        type: 'Microsoft.Batch/batchAccounts',
-        displayName: 'j1devbatchaccount',
-        _key: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount`,
-        id: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount`,
-        webLink: `https://portal.azure.com/#@www.fake-domain.com/resource/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount`,
-      }),
-    );
-  });
-
-  it('should collect an Azure Resource Group has Azure Batch Account relationship', () => {
-    const { collectedRelationships } = context.jobState;
-
-    expect(collectedRelationships).toContainEqual({
-      _class: 'HAS',
-      _fromEntityKey: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev`,
-      _key: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev|has|/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount`,
-      _toEntityKey: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount`,
-      _type: 'azure_resource_group_has_batch_account',
-      displayName: 'HAS',
-    });
-  });
+afterEach(async () => {
+  if (recording) {
+    await recording.stop();
+  }
 });
-
-describe('step - batch pools', () => {
-  beforeAll(async () => {
-    recording = setupAzureRecording({
-      directory: __dirname,
-      name: 'resource-manager-step-batch-pools',
-    });
-
-    instanceConfig = {
-      clientId: process.env.CLIENT_ID || 'clientId',
-      clientSecret: process.env.CLIENT_SECRET || 'clientSecret',
-      directoryId: '4a17becb-fb42-4633-b5c8-5ab66f28d195',
-      subscriptionId: '87f62f44-9dad-4284-a08f-f2fb3d8b528a',
-    };
-
-    const batchAccount = {
-      _key: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount`,
-      id:
-        '/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount',
-      _type: 'azure_batch_account',
-      _class: ['Service'],
-      name: 'j1devbatchaccount',
-    };
-
-    context = createMockAzureStepExecutionContext({
-      instanceConfig,
-      entities: [batchAccount],
-      setData: {
-        [ACCOUNT_ENTITY_TYPE]: { defaultDomain: 'www.fake-domain.com' },
-      },
-    });
-
-    await fetchBatchPools(context);
-  });
-
-  afterAll(async () => {
-    if (recording) {
-      await recording.stop();
-    }
-  });
-
-  it('should create an Azure Batch Pool entity', () => {
-    const { collectedEntities } = context.jobState;
-
-    expect(collectedEntities).toContainEqual(
-      expect.objectContaining({
-        _class: BatchEntities.BATCH_POOL._class,
-        _type: BatchEntities.BATCH_POOL._type,
-        _key: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount/pools/j1devbatchpool`,
-        id: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount/pools/j1devbatchpool`,
-        allocationState: 'Steady',
-        currentDedicatedNodes: 0,
-        currentLowPriorityNodes: 0,
-        interNodeCommunication: 'Disabled',
-        displayName: 'J1 Dev Batch Account Pool',
-        maxTasksPerNode: 1,
-        name: 'j1devbatchpool',
-        provisioningState: 'Succeeded',
-        type: 'Microsoft.Batch/batchAccounts/pools',
-        vmSize: 'STANDARD_A1',
-        webLink: `https://portal.azure.com/#@www.fake-domain.com/resource/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount/pools/j1devbatchpool`,
-      }),
-    );
-  });
-
-  it('should create an Azure Batch Account has Azure Batch Pool relationship', () => {
-    const { collectedRelationships } = context.jobState;
-
-    expect(collectedRelationships).toContainEqual({
-      _class: 'HAS',
-      _fromEntityKey: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount`,
-      _key: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount|has|/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount/pools/j1devbatchpool`,
-      _toEntityKey: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount/pools/j1devbatchpool`,
-      _type: 'azure_batch_account_has_pool',
-      displayName: 'HAS',
-    });
-  });
-});
-
-describe('step - batch applications', () => {
-  afterAll(async () => {
-    if (recording) {
-      await recording.stop();
-    }
-  });
-
-  test('rm-batch-applications', async () => {
-    const stepTestConfig = getStepTestConfigForStep(STEP_RM_BATCH_APPLICATION);
+test(
+  STEP_RM_BATCH_ACCOUNT,
+  async () => {
+    const stepTestConfig = getStepTestConfigForStep(STEP_RM_BATCH_ACCOUNT);
 
     recording = setupAzureRecording(
       {
-        name: 'rm-batch-applications',
+        name: STEP_RM_BATCH_ACCOUNT,
         directory: __dirname,
+        options: {
+          matchRequestsBy: getMatchRequestsBy({
+            config: stepTestConfig.instanceConfig,
+          }),
+        },
       },
       stepTestConfig.instanceConfig,
     );
 
     const stepResults = await executeStepWithDependencies(stepTestConfig);
     expect(stepResults).toMatchStepMetadata(stepTestConfig);
-  }, 10_000);
-});
+  },
+  100_000,
+);
+test(
+  STEP_RM_BATCH_POOL,
+  async () => {
+    const stepTestConfig = getStepTestConfigForStep(STEP_RM_BATCH_POOL);
 
-describe('step - batch certificates', () => {
-  beforeAll(async () => {
-    recording = setupAzureRecording({
-      directory: __dirname,
-      name: 'resource-manager-step-batch-certificates',
-    });
-
-    instanceConfig = {
-      clientId: process.env.CLIENT_ID || 'clientId',
-      clientSecret: process.env.CLIENT_SECRET || 'clientSecret',
-      directoryId: '4a17becb-fb42-4633-b5c8-5ab66f28d195',
-      subscriptionId: '87f62f44-9dad-4284-a08f-f2fb3d8b528a',
-    };
-
-    const batchAccount = {
-      _key: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount`,
-      id:
-        '/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount',
-      _type: 'azure_batch_account',
-      _class: ['Service'],
-      name: 'j1devbatchaccount',
-    };
-
-    context = createMockAzureStepExecutionContext({
-      instanceConfig,
-      entities: [batchAccount],
-      setData: {
-        [ACCOUNT_ENTITY_TYPE]: { defaultDomain: 'www.fake-domain.com' },
+    recording = setupAzureRecording(
+      {
+        name: STEP_RM_BATCH_POOL,
+        directory: __dirname,
+        options: {
+          matchRequestsBy: getMatchRequestsBy({
+            config: stepTestConfig.instanceConfig,
+          }),
+        },
       },
-    });
-
-    await fetchBatchCertificates(context);
-  });
-
-  afterAll(async () => {
-    if (recording) {
-      await recording.stop();
-    }
-  });
-
-  it('should collect an Azure Batch Certificate entity', () => {
-    const { collectedEntities } = context.jobState;
-
-    expect(collectedEntities).toContainEqual(
-      expect.objectContaining({
-        _type: BatchEntities.BATCH_CERTIFICATE._type,
-        _class: BatchEntities.BATCH_CERTIFICATE._class,
-        _key: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount/certificates/sha1-6820fee71b8312b01159d3c09fcb9f7ab24ae60d`,
-        id: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount/certificates/sha1-6820fee71b8312b01159d3c09fcb9f7ab24ae60d`,
-        displayName: 'sha1-6820fee71b8312b01159d3c09fcb9f7ab24ae60d',
-        format: 'Cer',
-        provisioningState: 'Succeeded',
-        type: 'Microsoft.Batch/batchAccounts/certificates',
-        thumbprint: expect.any(String),
-        thumbprintAlgorithm: 'sha1',
-        publicData: expect.any(String),
-        webLink: `https://portal.azure.com/#@www.fake-domain.com/resource/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount/certificates/sha1-6820fee71b8312b01159d3c09fcb9f7ab24ae60d`,
-      }),
+      stepTestConfig.instanceConfig,
     );
-  });
 
-  it('should collect an Azure Batch Account has Azure Batch Certificate relationship', () => {
-    const { collectedRelationships } = context.jobState;
+    const stepResults = await executeStepWithDependencies(stepTestConfig);
+    expect(stepResults).toMatchStepMetadata(stepTestConfig);
+  },
+  100_000,
+);
+test(
+  STEP_RM_BATCH_APPLICATION,
+  async () => {
+    const stepTestConfig = getStepTestConfigForStep(STEP_RM_BATCH_APPLICATION);
 
-    expect(collectedRelationships).toContainEqual({
-      _class: 'HAS',
-      _fromEntityKey: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount`,
-      _key: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount|has|/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount/certificates/sha1-6820fee71b8312b01159d3c09fcb9f7ab24ae60d`,
-      _toEntityKey: `/subscriptions/${instanceConfig.subscriptionId}/resourceGroups/j1dev/providers/Microsoft.Batch/batchAccounts/j1devbatchaccount/certificates/sha1-6820fee71b8312b01159d3c09fcb9f7ab24ae60d`,
-      _type: 'azure_batch_account_has_certificate',
-      displayName: 'HAS',
-    });
-  });
-});
+    recording = setupAzureRecording(
+      {
+        name: STEP_RM_BATCH_APPLICATION,
+        directory: __dirname,
+        options: {
+          matchRequestsBy: getMatchRequestsBy({
+            config: stepTestConfig.instanceConfig,
+          }),
+        },
+      },
+      stepTestConfig.instanceConfig,
+    );
+
+    const stepResults = await executeStepWithDependencies(stepTestConfig);
+    expect(stepResults).toMatchStepMetadata(stepTestConfig);
+  },
+  100_000,
+);
+test(
+  STEP_RM_BATCH_CERTIFICATE,
+  async () => {
+    const stepTestConfig = getStepTestConfigForStep(STEP_RM_BATCH_CERTIFICATE);
+
+    recording = setupAzureRecording(
+      {
+        name: STEP_RM_BATCH_CERTIFICATE,
+        directory: __dirname,
+        options: {
+          matchRequestsBy: getMatchRequestsBy({
+            config: stepTestConfig.instanceConfig,
+          }),
+        },
+      },
+      stepTestConfig.instanceConfig,
+    );
+
+    const stepResults = await executeStepWithDependencies(stepTestConfig);
+    expect(stepResults).toMatchStepMetadata(stepTestConfig);
+  },
+  100_000,
+);

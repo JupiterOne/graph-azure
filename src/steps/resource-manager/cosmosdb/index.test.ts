b11616d5a3855c80dc8b8f91815f4daec4f46537
Gonzalo Avalos Ribas <gonzaloavalosribas@Gonzalos-MacBook-Pro.local>
2024-01-29 12:09:28 -0300

    Cosmodb service

---
+++ src/steps/resource-manager/cosmosdb/index.test.ts
@@ -0,0 +1,43 @@
+import {
+  Recording,
+  executeStepWithDependencies,
+} from '@jupiterone/integration-sdk-testing';
+import { getStepTestConfigForStep } from '../../../../test/integrationInstanceConfig';
+import {
+  setupAzureRecording,
+  getMatchRequestsBy,
+} from '../../../../test/helpers/recording';
+import { STEP_RM_COSMOSDB_SQL_DATABASES } from './constants';
+
+let recording: Recording;
+
+afterEach(async () => {
+  if (recording) {
+    await recording.stop();
+  }
+});
+test(
+  STEP_RM_COSMOSDB_SQL_DATABASES,
+  async () => {
+    const stepTestConfig = getStepTestConfigForStep(
+      STEP_RM_COSMOSDB_SQL_DATABASES,
+    );
+
+    recording = setupAzureRecording(
+      {
+        name: STEP_RM_COSMOSDB_SQL_DATABASES,
+        directory: __dirname,
+        options: {
+          matchRequestsBy: getMatchRequestsBy({
+            config: stepTestConfig.instanceConfig,
+          }),
+        },
+      },
+      stepTestConfig.instanceConfig,
+    );
+
+    const stepResults = await executeStepWithDependencies(stepTestConfig);
+    expect(stepResults).toMatchStepMetadata(stepTestConfig);
+  },
+  100_000,
+);
